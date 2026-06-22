e658e5aceb5428f82e5a8150ba4d3123f076b9d7
accga12345 <tranduyson09092004@gmail.com>
2026-06-22 17:48:44 +0700

    fix: set DNS default result order to ipv4first for Render SMTP

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const dns = require('dns');
+try { dns.setDefaultResultOrder('ipv4first'); } catch (_) {}
+
 const { default: mongoose } = require('mongoose');
 const router = require('./routes');
 const bodyParser = require('body-parser');
